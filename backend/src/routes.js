7f1bb64c360c24e69b368d038b50456e115ad95e
Henrique Derosa <henriquederosa@Henriques-MacBook-Pro.local>
2020-03-26 21:20:06 -0300

    feat: add backend application

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const ongController = require('./controllers/OngController')
+const incidentController = require('./controllers/IncidentController')
+const profileController = require('./controllers/ProfileController')
+const sessionController = require('./controllers/SessionController')
+const routes = express.Router()
+
+routes.get('/ongs', ongController.index)
+routes.post('/ongs', ongController.create)
+
+routes.get('/incidents', incidentController.index)
+routes.post('/incidents', incidentController.create)
+routes.delete('/incidents/:id', incidentController.delete)
+
+routes.get('/profile', profileController.index)
+
+routes.get('/sessions', sessionController.create)
+
+module.exports = routes
